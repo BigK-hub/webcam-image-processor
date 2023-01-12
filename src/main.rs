745c74f0ae881c17e006e371ea71d0f352df715a
domjanabi <nathanbiesaga@gmail.com>
2023-01-12 21:07:44 +0100

    tinkering with box_blur params

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use camera_capture;
 use pixel_traits::*;
 
 const INPUT_MODE_NAMES: [&str; 3] = ["Normal", "TimeBlend", "Denoising"];
-const PROCESSOR_NAMES: [&str; 12] = ["Normal", "Sobel", "SobelColour", "Threshold", "ThresholdColour", "GaussianBlur", "BoxBlur", "GreyScale", "ChromaticAberration", "Sharpen", "SharpenColour", "CrossBlur"];
+const PROCESSOR_NAMES: [&str; 13] = ["Normal", "Sobel", "SobelColour", "Threshold", "ThresholdColour", "FloydSteinbergDithering", "GaussianBlur", "BoxBlur", "GreyScale", "ChromaticAberration", "Sharpen", "SharpenColour", "CrossBlur"];
 
 fn main()
 {
@@ -240,7 +240,7 @@ impl olc::PGEApplication for Window
                 Processor::ThresholdColour => self.frame.threshold_colour(&mut self.target, (pge.get_mouse_x()*255/ pge.screen_width() as i32) as u8),
                 Processor::FloydSteinbergDithering =>  self.frame.floyd_steinberg_dithering(&mut self.target, 1),
                 Processor::GaussianBlur => self.frame.gaussian_blur_3x3(&mut self.target),
-                Processor::BoxBlur => self.frame.box_blur(&mut self.target, (((pge.get_mouse_x()*255/ pge.screen_width() as i32 )/2)*2 + 1) as usize / 50),//.min((pge.screen_width() as i32/2)*2 - 1).min(39) as usize),
+                Processor::BoxBlur => self.frame.box_blur(&mut self.target, ((((pge.get_mouse_x() * 255 / pge.screen_width() as i32 )/2)*2 + 1) as usize).min((pge.screen_width()/2)*2 - 1).max(3)),
                 Processor::GreyScale => self.frame.greyscale(&mut self.target),
                 Processor::ChromaticAberration => self.frame.chromatic_aberration(&mut self.target, (pge.get_mouse_x() as usize * 255/ pge.screen_width())/20),
                 Processor::Sharpen => self.frame.sharpen(&mut self.target),
